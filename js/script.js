ef35ca00c86eb4e8e8804a2610fd885e178a655c
Fabio Aurora <fabioauroradev@gmail.com>
2022-05-25 21:53:12 +0100

    fixed bug

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ addBTN.addEventListener("click", () => {
 });
 
 // searching for contact number:
-searchBTN.addEventListener('click', () => {
+searchBTN.addEventListener('touch', () => {
     for (let contact of contacts) {
         const splitString = contact.split(':');
         searchContact.value = searchContact.value.toLowerCase();
