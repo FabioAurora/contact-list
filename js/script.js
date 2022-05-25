8f2556ddc7fc5e32b01310de4dd154bc0979b67d
Fabio Aurora <fabioauroradev@gmail.com>
2022-05-25 23:25:05 +0100

    fixed bug on mobile version 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict"; // strict mode
 
 // Targeting the nodes:
-var addName = document.querySelector("#contactName");
-var addNumber = document.querySelector("#contactNumber");
+let addName = document.querySelector("#contactName");
+let addNumber = document.querySelector("#contactNumber");
 const addBTN = document.querySelector("#addContactBTN");
 
 const contactList = document.querySelector("#contactList");
 
-const contacts = [];
+let contacts = [];
 
 const searchContact = document.querySelector("#searchContact");
 const searchBTN = document.querySelector("#searchBTN");
@@ -33,7 +33,7 @@ addBTN.addEventListener("click", () => {
 
 // searching for contact number:
 searchBTN.addEventListener('click', () => {
-    for (var contact of contacts) {
+    for (let contact of contacts) {
         const splitString = contact.split(':');
         searchContact.value = searchContact.value.toLowerCase();
 
